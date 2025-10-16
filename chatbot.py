4f3ad1bfc7596419921d169afbe05c58abc271de
arnabd64 <arnabdhar32@gmail.com>
2025-10-16 08:03:14 +0000

    Updated Documentation

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,6 @@
-import os
 import json
+import os
+
 import dotenv
 import httpx
 
@@ -12,40 +13,77 @@ BACKUP_HISTORY_URL = os.getenv("BACKUP_HISTORY_URL")
 
 
 class Chatbot:
+    """
+    Class that abstracts logic from the Chainlit
+    application. Each chat session has it's own
+    instance of this class which can be identified
+    using their `session_id`.
+
+    Attributes:
+        - `session_id` (str) : Unique Identifier for each session
+    """
     def __init__(self, session_id: str):
         self.session_id = session_id
 
     @classmethod
     async def new_session(cls):
+        """
+        Factory function that initiates a new
+        session by deriving a session id from n8n
+        """
         async with httpx.AsyncClient() as client:
             response = await client.get(SESSION_ID_URL)
             response.raise_for_status()
         return Chatbot(response.text)
 
-    async def send_message(self, message: str) -> str:
+    async def send_message(self, user_message: str) -> str:
+        """
+        Function that is Triggered after the user
+        inputs a new message in the chainlit application.
+
+        Attributes:
+            - `message` (str) : User's Message
+
+        Returns:
+            - `ai_message` (str) : LLM's Message
+        """
         async with httpx.AsyncClient() as client:
             response = await client.post(
                 url=CHAT_WORKFLOW_URL,
-                json={"sessionId": self.session_id, "chatInput": message},
+                json={"sessionId": self.session_id, "chatInput": user_message},
                 timeout=300,
             )
             response.raise_for_status()
         return response.json()[0]["output"]
-    
+
     async def get_chat_history(self) -> list[dict]:
+        """
+        Retrieves the chat history from redis database
+        used to save chat message history for a particular
+        session.
+
+        Returns:
+            - `chat_history` (list) : Chat History for `session_id`
+        """
         async with httpx.AsyncClient() as client:
             response = await client.get(
-                url = f"{CHAT_HISTORY_URL}?sessionId={self.session_id}",
-                timeout=300
+                url=f"{CHAT_HISTORY_URL}?sessionId={self.session_id}", timeout=300
             )
             response.raise_for_status()
         return [json.loads(item) for item in response.json()["history"]]
-    
+
     async def backup_chat_history(self, history: list[dict]):
+        """
+        Executes the Backup-Chat-History workflow on n8n,
+        which backs up chat history from redis to mongo db
+
+        Attributes:
+            - `history` (list) : Chat History
+        """
         async with httpx.AsyncClient() as client:
             response = await client.post(
-                url = f"{BACKUP_HISTORY_URL}?sessionId={self.session_id}",
-                json = {"history": history},
-                timeout = 300
+                url=f"{BACKUP_HISTORY_URL}?sessionId={self.session_id}",
+                json={"history": history},
+                timeout=300,
             )
             response.raise_for_status()
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+"""
+Chainlit Application
+"""
 import chainlit as ct
 
 from chatbot import Chatbot
@@ -5,25 +8,42 @@ from chatbot import Chatbot
 
 @ct.password_auth_callback
 async def password_authentication(username: str, password: str):
+    """
+    Adds a login screen to the application
+    """
     if (username, password) == ("admin", "admin"):
         return ct.User(username)
 
 
 @ct.on_chat_start
 async def init_chatbot():
+    """
+    Method to execute when a new chat session has
+    been initiated.
+    """
     chatbot = await Chatbot.new_session()
     ct.user_session.set("id", chatbot.session_id)
     ct.user_session.set("chatbot", chatbot)
 
+
 @ct.on_chat_end
 async def fetch_chat_history():
+    """
+    Method to execute when an existing chat session
+    has been terminated.
+    """
     chatbot: Chatbot = ct.user_session.get("chatbot")
     chat_history = await chatbot.get_chat_history()
     if len(chat_history) > 0:
         await chatbot.backup_chat_history(chat_history)
 
+
 @ct.on_message
 async def chat_workflow(message: ct.Message):
+    """
+    Method to execute when the user inputs a
+    new chat message.
+    """
     # get chatbot instance
     chatbot: Chatbot = ct.user_session.get("chatbot")
 
